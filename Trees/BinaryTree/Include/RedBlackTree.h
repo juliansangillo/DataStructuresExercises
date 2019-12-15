c5cde78734f2946856611fa04f3dbfd11877f3b6
Julian Sangillo <44471531+juliansangillo@users.noreply.github.com>
2019-12-15 03:45:42 -0500

    Overrided RedBlackTree insert()

--- Trees/BinaryTree/Include/RedBlackTree.h
+++ Trees/BinaryTree/Include/RedBlackTree.h
@@ -11,7 +11,7 @@ class RedBlackNode : public Node<T> {
     private:
         bool color;
     public:
-        RedBlackNode(T data) : BinaryTree<T>(data) {
+        RedBlackNode(T data) : Node<T>(data) {
 
             this->color = RED;
 
@@ -33,12 +33,187 @@ class RedBlackNode : public Node<T> {
 
 template<class T>
 class RedBlackTree : public BinaryTree<T> {
+    private:
+        void insertNode(RedBlackNode<T>* current, T data) {
+
+            if(data < current->get()) {
+                if(current->left == NULL) {
+                    current->left = new RedBlackNode<T>(data);
+                    return;
+                }
+
+                insertNode((RedBlackNode<T>*)current->left, data);
+            }
+            else if(data > current->get()) {
+                if(current->right == NULL) {
+                    current->right = new RedBlackNode<T>(data);
+                    return;
+                }
+
+                insertNode((RedBlackNode<T>*)current->right, data);
+            }
+            else 
+                throw duplicate_value_exception();
+
+        }
+
+        int isNotRedBlack(RedBlackNode<T>* tree) {
+
+            if(tree->getColor() == BLACK) {
+                if(tree->left != NULL && static_cast<RedBlackNode<T>*>(tree->left)->getColor() == RED)
+                    if(tree->left->left != NULL && static_cast<RedBlackNode<T>*>(tree->left->left)->getColor() == RED)
+                        return 1;
+                    else if(tree->left->right != NULL && static_cast<RedBlackNode<T>*>(tree->left->right)->getColor() == RED)
+                        return 2;
+                
+                if(tree->right != NULL && static_cast<RedBlackNode<T>*>(tree->right)->getColor() == RED)
+                    if(tree->right->left != NULL && static_cast<RedBlackNode<T>*>(tree->right->left)->getColor() == RED)
+                        return 3;
+                    else if(tree->right->right != NULL && static_cast<RedBlackNode<T>*>(tree->right->right)->getColor() == RED)
+                        return 4;
+            }
+
+            return 0;
+        }
+
+        void pushColorLeft(RedBlackNode<T>* node) {
+
+            node->recolor();
+            static_cast<RedBlackNode<T>*>(node->left)->recolor();
+
+        }
+
+        void pushColorRight(RedBlackNode<T>* node) {
+
+            node->recolor();
+            static_cast<RedBlackNode<T>*>(node->right)->recolor();
+
+        }
+
+        void pushColorDown(RedBlackNode<T>* node) {
+
+            node->recolor();
+            static_cast<RedBlackNode<T>*>(node->left)->recolor();
+            static_cast<RedBlackNode<T>*>(node->right)->recolor();
+
+        }
+
+        RedBlackNode<T>* rotateLeft(RedBlackNode<T>* currentRoot) {
+
+            RedBlackNode<T>* newRoot = (RedBlackNode<T>*)currentRoot->right;
+
+            currentRoot->right = newRoot->left;
+            newRoot->left = currentRoot;
+
+            return newRoot;
+        }
+
+        RedBlackNode<T>* rotateRight(RedBlackNode<T>* currentRoot) {
+
+            RedBlackNode<T>* newRoot = (RedBlackNode<T>*)currentRoot->left;
+
+            currentRoot->left = newRoot->right;
+            newRoot->right = currentRoot;
+
+            return newRoot;
+        }
+
+        RedBlackNode<T>* rotateLeftRight(RedBlackNode<T>* currentRoot) {
+
+            currentRoot->left = rotateLeft((RedBlackNode<T>*)currentRoot->left);
+            RedBlackNode<T>* newRoot = rotateRight(currentRoot);
+
+            return newRoot;
+        }
+
+        RedBlackNode<T>* rotateRightLeft(RedBlackNode<T>* currentRoot) {
+
+            currentRoot->right = rotateRight((RedBlackNode<T>*)currentRoot->right);
+            RedBlackNode<T>* newRoot = rotateLeft(currentRoot);
+            
+            return newRoot;
+        }
+
+        RedBlackNode<T>* correctSubTree(RedBlackNode<T>* tree, int status) {
+
+            RedBlackNode<T>* newRoot = tree;
+
+            switch(status) {
+                case 1:
+                    if(tree->right == NULL || static_cast<RedBlackNode<T>*>(tree->right)->getColor() == BLACK) {
+                        newRoot = rotateRight(tree);
+                        pushColorRight(newRoot);
+                    }
+                    else
+                        pushColorDown(tree);
+                    break;
+                case 2:
+                    if(tree->right == NULL || static_cast<RedBlackNode<T>*>(tree->right)->getColor() == BLACK) {
+                        newRoot = rotateLeftRight(tree);
+                        pushColorRight(newRoot);
+                    }
+                    else
+                        pushColorDown(tree);
+                    break;
+                case 3:
+                    if(tree->left == NULL || static_cast<RedBlackNode<T>*>(tree->left)->getColor() == BLACK) {
+                        newRoot = rotateRightLeft(tree);
+                        pushColorLeft(newRoot);
+                    }
+                    else
+                        pushColorDown(tree);
+                    break;
+                case 4:
+                    if(tree->left == NULL || static_cast<RedBlackNode<T>*>(tree->left)->getColor() == BLACK) {
+                        newRoot = rotateLeft(tree);
+                        pushColorLeft(newRoot);
+                    }
+                    else 
+                        pushColorDown(tree);
+                    break;
+            }
+
+            return newRoot;
+        }
+
+        void evaluateSubTree(RedBlackNode<T>* tree) {
+
+            if(tree->left != NULL) {
+                evaluateSubTree((RedBlackNode<T>*)tree->left);
+
+                if(int status = isNotRedBlack((RedBlackNode<T>*)tree->left))
+                    tree->left = correctSubTree((RedBlackNode<T>*)tree->left, status);
+            }
+
+            if(tree->right != NULL) {
+                evaluateSubTree((RedBlackNode<T>*)tree->right);
+
+                if(int status = isNotRedBlack((RedBlackNode<T>*)tree->right))
+                    tree->right = correctSubTree((RedBlackNode<T>*)tree->right, status);
+            }
+
+        }
     public:
-        RedBlackTree() : BinaryTree<T> {}
+        RedBlackTree() : BinaryTree<T>() {}
 
         ~RedBlackTree() {}
 
-        void insert(T data) {}
+        void insert(T data) {
+
+            if(this->root == NULL)
+                this->root = new RedBlackNode<T>(data);
+            else
+                insertNode((RedBlackNode<T>*)this->root, data);
+
+            evaluateSubTree((RedBlackNode<T>*)this->root);
+
+            if(int status = isNotRedBlack((RedBlackNode<T>*)this->root))
+                    this->root = correctSubTree((RedBlackNode<T>*)this->root, status);
+
+            if(static_cast<RedBlackNode<T>*>(this->root)->getColor() == RED)
+                static_cast<RedBlackNode<T>*>(this->root)->recolor();
+
+        }
 
         void remove(T data) {}
 
@@ -61,7 +236,7 @@ class RedBlackTree : public BinaryTree<T> {
             for(int i = 0; i < level; i++)
                 std::cout << "\t";
             std::cout << "Color: ";
-            if(root->color == BLACK)
+            if(root->getColor() == BLACK)
                 std::cout << "BLACK" << std::endl;
             else
                 std::cout << "RED" << std::endl;
--- Trees/BinaryTree/Source/Driver.cpp
+++ Trees/BinaryTree/Source/Driver.cpp
@@ -7,7 +7,91 @@ using namespace std;
 
 int main() {
 
-    
+    RedBlackTree<int> tree;
+
+    tree.insert(50);
+    tree.print();
+    cout << endl;
+
+    tree.insert(40);
+    tree.print();
+    cout << endl;
+
+    tree.insert(60);
+    tree.print();
+    cout << endl;
+
+    tree.insert(20);
+    tree.print();
+    cout << endl;
+
+    tree.insert(45);
+    tree.print();
+    cout << endl;
+
+    tree.insert(55);
+    tree.print();
+    cout << endl;
+
+    tree.insert(70);
+    tree.print();
+    cout << endl;
+
+    tree.insert(15);
+    tree.print();
+    cout << endl;
+
+    tree.insert(25);
+    tree.print();
+    cout << endl;
+
+    tree.insert(52);
+    tree.print();
+    cout << endl;
+
+    tree.insert(58);
+    tree.print();
+    cout << endl;
+
+    tree.insert(80);
+    tree.print();
+    cout << endl;
+
+    tree.insert(56);
+    tree.print();
+    cout << endl;
+
+    tree.insert(100);
+    tree.print();
+    cout << endl;
+
+    tree.insert(51);
+    tree.print();
+    cout << endl;
+
+    tree.insert(54);
+    tree.print();
+    cout << endl;
+
+    tree.insert(53);
+    tree.print();
+    cout << endl;
+
+    tree.insert(57);
+    tree.print();
+    cout << endl;
+
+    tree.insert(19);
+    tree.print();
+    cout << endl;
+
+    tree.insert(16);
+    tree.print();
+    cout << endl;
+
+    tree.insert(10);
+    tree.print();
+    cout << endl;
     
     return 0;
 }
