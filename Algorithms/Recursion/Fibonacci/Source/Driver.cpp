685e5492d964d4122ff20532d6b2884234c6ce0f
Julian Sangillo <44471531+juliansangillo@users.noreply.github.com>
2019-12-30 18:52:30 -0500

    Initial Commit

---
+++ Algorithms/Recursion/Fibonacci/Source/Driver.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int fibonacciIterative(int);
+int fibonacciRecursive(int);
+
+int main() {
+
+    cout << "Iterative: " << fibonacciIterative(2) << endl;
+    cout << "Recursive: " << fibonacciRecursive(2) << endl;
+
+    return 0;
+}
+
+int fibonacciIterative(int num) {
+
+    //TODO Iterative code goes here
+
+    return 0;
+}
+
+int fibonacciRecursive(int num) {
+
+    //TODO Recursive code goes here
+
+    return 0;
+}
