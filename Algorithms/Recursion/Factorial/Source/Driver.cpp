56125d910087deb4cbff8d585c65f82622991df7
Julian Sangillo <44471531+juliansangillo@users.noreply.github.com>
2019-12-30 17:11:34 -0500

    Initial Commit

---
+++ Algorithms/Recursion/Factorial/Source/Driver.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int factorialIterative(int);
+int factorialRecursive(int);
+
+int main() {
+
+    cout << "Iterative: " << factorialIterative(5) << endl;
+    cout << "Recursive: " << factorialRecursive(5) << endl;
+
+    return 0;
+}
+
+int factorialIterative(int num) {
+
+    //TODO Iterative code goes here
+    
+    return 0;
+}
+
+int factorialRecursive(int num) {
+
+    //TODO Recursive code goes here
+
+    return 0;
+}
