c6e7e73e940b59b785cec045d6e0578e5b056658
Julian Sangillo <44471531+juliansangillo@users.noreply.github.com>
2020-01-04 16:32:57 -0500

    Added vector<T> bubbleSort(vector<T>)

--- Algorithms/Sorting/BubbleSort/Source/Driver.cpp
+++ Algorithms/Sorting/BubbleSort/Source/Driver.cpp
@@ -8,7 +8,7 @@ vector<T> bubbleSort(vector<T>);
 
 int main() {
 
-    vector<int> numbers = {6, 5, 3, 1, 8, 7, 2, 4};
+    vector<int> numbers = {6, 5, 3, 1, 8, 7, 2, 4, 20, -5};
 
     numbers = bubbleSort(numbers);
 
@@ -24,7 +24,15 @@ int main() {
 template<typename T>
 vector<T> bubbleSort(vector<T> array) {
 
-    //Perform bubble sort on array
+    typename vector<T>::iterator pos;
+    for(int sorted = 0; sorted < array.size(); sorted++)
+        for(pos = array.begin(); pos < array.end() - (sorted + 1); pos++) {
+            if(*pos > *(pos + 1)) {
+                T temp = *pos;
+                *pos = *(pos + 1);
+                *(pos + 1) = temp;
+            }
+        }
 
     return array;
 }
