9388ed0bc5edd7acbc6894b399afc867ccd14f04
Julian Sangillo <44471531+juliansangillo@users.noreply.github.com>
2020-01-04 20:02:16 -0500

    Changed selectionSort() input

--- Algorithms/Sorting/SelectionSort/Source/Driver.cpp
+++ Algorithms/Sorting/SelectionSort/Source/Driver.cpp
@@ -12,7 +12,7 @@ vector<T> selectionSort(vector<T>);
 
 int main() {
 
-    vector<int> numbers = {6, 5, 3, 1, 8, 7, 2, 4};
+    vector<int> numbers = {99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0};
 
     numbers = selectionSort(numbers);
 
