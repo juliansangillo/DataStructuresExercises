b96f4f212286147e9cd4b484e2200784d4fce58f
Julian Sangillo <44471531+juliansangillo@users.noreply.github.com>
2020-01-11 18:35:16 -0500

    Added vector<T> mergeSort(vector<T>)

--- Algorithms/Sorting/MergeSort/Source/Driver.cpp
+++ Algorithms/Sorting/MergeSort/Source/Driver.cpp
@@ -28,14 +28,22 @@ int main() {
 template<typename T>
 vector<T> mergeSort(vector<T> array) {
 
+    if(array.size() == 1)
+        return array;
 
-    return array;
+    vector<T> left, right;
+    split(array, left, right);
+
+    return merge(mergeSort(left), mergeSort(right));
 }
 
 template<typename T>
 void split(vector<T> array, vector<T>& left, vector<T>& right) {
 
+    int leftSize = array.size() / 2;
 
+    left = vector<T>(array.begin(), array.begin() + leftSize);
+    right = vector<T>(array.begin() + leftSize, array.end());
 
 }
 
@@ -43,6 +51,26 @@ template<typename T>
 vector<T> merge(vector<T> left, vector<T> right) {
 
     vector<T> array;
+
+    typename vector<T>::iterator l = left.begin();
+    typename vector<T>::iterator r = right.begin();
+
+    while(l != left.end() && r != right.end()) {
+        if(*r < *l) {
+            array.push_back(*r);
+            r++;
+        }
+        else {
+            array.push_back(*l);
+            l++;
+        }
+    }
+
+    for(; l != left.end(); l++)
+        array.push_back(*l);
+
+    for(; r != right.end(); r++)
+        array.push_back(*r);
 
     return array;
 }
